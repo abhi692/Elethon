f89b61740244b19336f8db763a4df081390e8258
Abhishek Kumar <abhiroshan692@gmail.com>
2023-03-19 19:26:08 +0530

    SIDEPANEL updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,45 +5,49 @@ import elewayte_logo from '../assests/elewayte_logo.png';
 import 'boxicons';
 
 const Home = () => {
-  // const [state, setState] = useState(false);
-  // const setState = () =>{
 
-  // }
+    let btn= document.querySelector("#btn");
+    let sidebar = document.querySelector(".sidebar");
 
+    window.onload = function(){ 
+      btn.onclick = function() {
+      sidebar.classList.toggle("active");
+  };
+    }
 
   return (
     <>
     <div className="container" style ={ { backgroundImage: "url('background for elethon.png')" } }>
-    <div className="sidebar">
+      <div className="sidebar">
       <div className="logo_content">
         <div className="logo">
           <div className="logo_name"></div>
-          <i class='bx bx-menu' id='btn'></i><br/>
+          <i className='bx bx-menu' id='btn'></i><br/>
           <ul className='nav_list'>
             <li>
               <a href='#'> 
-              <i class='bx bxs-calendar'></i>
+              <i className='bx bxs-calendar'></i>
               <span className='links_name'> Schedule</span>
               </a>
               <span className='tooltip'>Schedule</span>
             </li>
             <li>
               <a href='#'>
-              <i class='bx bxl-blogger'></i>
+              <i className='bx bxl-blogger'></i>
               <span className='links_name'> Blogs</span>
               </a>
               <span className='tooltip'>Blogs</span>
             </li>
             <li>
               <a href='#'>
-              <i class='bx bx-trophy'></i>
+              <i className='bx bx-trophy'></i>
               <span className='links_name'> FAQ & Prizes</span>
               </a>
               <span className='tooltip'>FAQ & Prizes</span>
             </li>
             <li>
               <a href='#'>
-              <i class='bx bxs-calendar'></i>
+              <i className='bx bxs-calendar'></i>
               <span className='links_name'> Register</span>
               </a>
               <span className='tooltip'>Register</span>
